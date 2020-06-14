b5735a76608889e006540ea5518b501d49e267dd
Mariappan Ramasamy <rmariappan@outlook.com>
2020-06-14 14:50:03 +0800

    Fixed the linker script to use stmduino ld

--- keyboards/handwired/matreus42/blackpill_f103/rules.mk
+++ keyboards/handwired/matreus42/blackpill_f103/rules.mk
@@ -6,7 +6,7 @@ MCU_SERIES = STM32F1xx
 # linker script to use
 # it should exist either in <chibios>/os/common/ports/ARMCMx/compilers/GCC/ld/
 #  or <this_dir>/ld/
-MCU_LDSCRIPT = STM32F103x8
+MCU_LDSCRIPT = STM32F103x8_stm32duino_bootloader
 # startup code to use
 # is should exist in <chibios>/os/common/startup/ARMCMx/compilers/GCC
 MCU_STARTUP = stm32f1xx
@@ -20,11 +20,11 @@ ARMV = 7
 USE_FPU = no
 
 # Address of the booloader in system memory
-# STM32_BOOTLOADER_ADDRESS ?= 0x80000000
+STM32_BOOTLOADER_ADDRESS ?= 0x08000000
 
 # Options to pass to dfu-util when flashing
-# DFU_ARGS = -d 1eaf:0003 -a 2 -s 0x08000000:leave
-# DFU_SUFFIX_ARGS = -v 1eaf -p 0003
+DFU_ARGS = -d 1eaf:0003 -a 2
+DFU_SUFFIX_ARGS = -v 1eaf -p 0003
 
 # Build Options
 #   change yes to no to disable
