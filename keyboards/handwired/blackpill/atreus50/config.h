279b93faa5f5b493e44fda8da3b4b145888f1a46
Mariappan Ramasamy <rmariappan@outlook.com>
2020-06-29 16:20:01 +0800

    Working condition

--- keyboards/handwired/blackpill/atreus50/config.h
+++ keyboards/handwired/blackpill/atreus50/config.h
@@ -14,6 +14,7 @@
 #define MATRIX_COLS 13
 #define DIODE_DIRECTION COL2ROW
 
-#define MATRIX_ROW_PINS { A0, A1, A3, A2 }
-#define MATRIX_COL_PINS { B13, B14, B15, A8, A9, A10, A4, A5, A6, A7, B1, B0, B12 }
+#define MATRIX_ROW_PINS { A7, A6, A5, A4 }
+// #define MATRIX_COL_PINS { B13, B14, B15, A8, A9, A10, A7, A3, A4, A5, A6, B1, B0 }
+#define MATRIX_COL_PINS { C13, C14, C15, A0, A1, A2, A3, B5, A15, B3, B4, B8, B9 }
 #define UNUSED_PINS
